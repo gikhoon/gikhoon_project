7b59767da6af5ba2388e1f63b78d4989e480109f
gikhoon@soongsil.ac.kr <gikhoon@soongsil.ac.kr>
2022-06-22 12:01:51 +0900

    0622 12:00 desk

---
+++ src/BaekJoon10845/Main.java
@@ -0,0 +1,7 @@
+package BaekJoon10845;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Baekjoon/Q1072.java
@@ -0,0 +1,13 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Q1072 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int percent = (int)(((double)y/x)*100);
+
+    }
+}
